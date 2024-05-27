8760b529e0e3b573eefd4c5db0691c5b42dbb64a
hyeri-jung <hyeri.jung@navercorp.com>
2024-05-27 18:24:37 +0900

    section2. 16.service 계층을 kotlin으로 변ㅇ경 -UserService

--- src/main/kotlin/com/group/libraryapp/domain/user/UserRepository.kt
+++ src/main/kotlin/com/group/libraryapp/domain/user/UserRepository.kt
@@ -3,6 +3,6 @@ package com.group.libraryapp.domain.user
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.*
 
-interface UserRepository : JpaRepository<User, Long> {
-    fun findByName(userName: String): Optional<User>
+interface UserRepository : JpaRepository<JavaUser, Long> {
+    fun findByName(userName: String): Optional<JavaUser>
 }
