0cd7884ba6985306e67f8b4dba63c7f9207ad822
hyeri-jung <hyeri.jung@navercorp.com>
2024-05-28 13:53:19 +0900

    section12-19 controller를 kotlin으로 변경하기

--- src/main/java/com/group/libraryapp/controller/book/BookController.java
+++ src/main/java/com/group/libraryapp/controller/book/BookController.java
@@ -1,36 +1,36 @@
-package com.group.libraryapp.controller.book;
-
-import com.group.libraryapp.dto.book.BookLoanRequest;
-import com.group.libraryapp.dto.book.BookRequest;
-import com.group.libraryapp.dto.book.BookReturnRequest;
-import com.group.libraryapp.service.book.JavaBookService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class BookController {
-
-  private final JavaBookService javaBookService;
-
-  public BookController(JavaBookService javaBookService) {
-    this.javaBookService = javaBookService;
-  }
-
-  @PostMapping("/book")
-  public void saveBook(@RequestBody BookRequest request) {
-    javaBookService.saveBook(request);
-  }
-
-  @PostMapping("/book/loan")
-  public void loanBook(@RequestBody BookLoanRequest request) {
-    javaBookService.loanBook(request);
-  }
-
-  @PutMapping("/book/return")
-  public void returnBook(@RequestBody BookReturnRequest request) {
-    javaBookService.returnBook(request);
-  }
-
-}
+// package com.group.libraryapp.controller.book;
+//
+// import com.group.libraryapp.dto.book.BookLoanRequest;
+// import com.group.libraryapp.dto.book.BookRequest;
+// import com.group.libraryapp.dto.book.BookReturnRequest;
+// import com.group.libraryapp.service.book.JavaBookService;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.PutMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.RestController;
+//
+// @RestController
+// public class BookController {
+//
+//   private final JavaBookService javaBookService;
+//
+//   public BookController(JavaBookService javaBookService) {
+//     this.javaBookService = javaBookService;
+//   }
+//
+//   @PostMapping("/book")
+//   public void saveBook(@RequestBody BookRequest request) {
+//     javaBookService.saveBook(request);
+//   }
+//
+//   @PostMapping("/book/loan")
+//   public void loanBook(@RequestBody BookLoanRequest request) {
+//     javaBookService.loanBook(request);
+//   }
+//
+//   @PutMapping("/book/return")
+//   public void returnBook(@RequestBody BookReturnRequest request) {
+//     javaBookService.returnBook(request);
+//   }
+//
+// }
--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -1,40 +1,40 @@
-package com.group.libraryapp.controller.user;
-
-import com.group.libraryapp.dto.user.UserCreateRequest;
-import com.group.libraryapp.dto.user.UserUpdateRequest;
-import com.group.libraryapp.dto.user.UserResponse;
-import com.group.libraryapp.service.user.JavaUserService;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-public class UserController {
-
-  private final JavaUserService javaUserService;
-
-  public UserController(JavaUserService javaUserService) {
-    this.javaUserService = javaUserService;
-  }
-
-  @PostMapping("/user")
-  public void saveUser(@RequestBody UserCreateRequest request) {
-    javaUserService.saveUser(request);
-  }
-
-  @GetMapping("/user")
-  public List<UserResponse> getUsers() {
-    return javaUserService.getUsers();
-  }
-
-  @PutMapping("/user")
-  public void updateUserName(@RequestBody UserUpdateRequest request) {
-    javaUserService.updateUserName(request);
-  }
-
-  @DeleteMapping("/user")
-  public void deleteUser(@RequestParam String name) {
-    javaUserService.deleteUser(name);
-  }
-
-}
+// package com.group.libraryapp.controller.user;
+//
+// import com.group.libraryapp.dto.user.UserCreateRequest;
+// import com.group.libraryapp.dto.user.UserUpdateRequest;
+// import com.group.libraryapp.dto.user.UserResponse;
+// import com.group.libraryapp.service.user.JavaUserService;
+// import org.springframework.web.bind.annotation.*;
+//
+// import java.util.List;
+//
+// @RestController
+// public class UserController {
+//
+//   private final JavaUserService javaUserService;
+//
+//   public UserController(JavaUserService javaUserService) {
+//     this.javaUserService = javaUserService;
+//   }
+//
+//   @PostMapping("/user")
+//   public void saveUser(@RequestBody UserCreateRequest request) {
+//     javaUserService.saveUser(request);
+//   }
+//
+//   @GetMapping("/user")
+//   public List<UserResponse> getUsers() {
+//     return javaUserService.getUsers();
+//   }
+//
+//   @PutMapping("/user")
+//   public void updateUserName(@RequestBody UserUpdateRequest request) {
+//     javaUserService.updateUserName(request);
+//   }
+//
+//   @DeleteMapping("/user")
+//   public void deleteUser(@RequestParam String name) {
+//     javaUserService.deleteUser(name);
+//   }
+//
+// }
