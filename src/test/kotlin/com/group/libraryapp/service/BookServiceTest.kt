d8d8e171fb9c9e0e7c41aba8d257ec82b9ab8724
hyeri-jung <hyeri.jung@worksmobile.com>
2024-05-29 16:19:50 +0900

    21.책의 분야 추가하기

--- src/test/kotlin/com/group/libraryapp/service/BookServiceTest.kt
+++ src/test/kotlin/com/group/libraryapp/service/BookServiceTest.kt
@@ -3,7 +3,11 @@ package com.group.libraryapp.service
 import com.group.libraryapp.domain.book.BookRepository
 import com.group.libraryapp.domain.user.UserRepository
 import com.group.libraryapp.domain.user.loanhistory.UserLoanHistoryRepository
+import com.group.libraryapp.dto.book.BookRequest
 import com.group.libraryapp.service.book.BookService
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
@@ -14,5 +18,22 @@ class BookServiceTest @Autowired constructor(
         private val userRepository: UserRepository,
         private val userLoanHistoryRepository: UserLoanHistoryRepository,
 ) {
+
+    @Test
+    @DisplayName("책 등록이 정상 동작한다.")
+    fun saveBookTest() {
+        //given
+        val request = BookRequest("이상한 나라의 앨리스", "SOCIETY")
+
+        //when
+        bookService.saveBook(request)
+
+        //then
+        val books = bookRepository.findAll()
+        assertThat(books).hasSize(1)
+        assertThat(books[0].name).isEqualTo("이상한 나라의 앨리스")
+        assertThat(books[0].type).isEqualTo("SOCIETY")
+    }
+
 
 }
